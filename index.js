b980cf66f4155b17ade0f3d358a964b7f3f09e0d
Hamilton Gramacho <hamiltongramacho@gmail.com>
2024-09-05 15:53:41 -0300

    código comentado

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const rl = readline.createInterface({ //nesta linha cria-se a constante rl que s
     output: process.stdout
 })
 
-
+//Função com a primeira pergunta via readline
 
 function definirNome(){
-    return new Promise( ( resolve, reject ) => {
-        rl.question( 'Qual seu nome? ', ( nome ) => {
-            resolve( nome )
+    return new Promise( ( resolve, reject ) => { //Estabelecida a Promisse que lida com a assincronia 
+        rl.question( 'Qual seu nome? ', ( nome ) => { //Pergunta via readline, resposta armazenada na variável
+            resolve( nome )//'resolve'é o comando de retorno da função caso ela seja atendida positivamente
             })
         })
     }
 
-function bomDia(nome){
-    console.log("bom dia " + nome)
+function bomDia(nomeDefinido){ //Função 
+    console.log("bom dia " + nomeDefinido)
     rl.close()
 }
 
 
-definirNome().then( nome => { 
-    bomDia(nome)
+definirNome().then( nomeExibido => { 
+    bomDia(nomeExibido)
     rl.close()
 })
 
